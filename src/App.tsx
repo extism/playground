2c1490f0c09abb9fd8aeff59ea536ac3a5f63ddf
Jhaines1988 <josephthaines@gmail.com>
2022-11-07 14:40:33 -0700

    chore: initialize repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ExtismContext from "@extism/runtime-browser";
+console.dir(ExtismContext);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.f
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
